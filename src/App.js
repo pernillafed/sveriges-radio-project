e97a52449635ab0bd3cce2ef2473eda792cfbcce
Pernilla <coolanilla@hotmail.com>
2021-04-21 16:08:49 +0200

    Installed amazing proxy and created Home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Route exact path="/" component={Home} />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            <h1>Home works</h1>
+        </div>
+    );
+}
+
+export default Home;
